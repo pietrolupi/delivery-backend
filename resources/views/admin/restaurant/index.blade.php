2604905af59577e02ee0d812741955f1ed19a88b
franedsandi <franedsandi@gmail.com>
2024-01-14 20:24:14 +0100

    [fix] profile bug

--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -3,12 +3,14 @@
 @section('content')
 
 @if(isset($restaurant->user_id))
-<div class="card" style="width: 18rem;">
-    <img src="{{ asset('storage/' . $restaurant->image) }}" class="card-img-top" alt="...">
-    <div class="card-body">
-        <h2 class="card-text">{{$restaurant->name}}</h2>
+<a href="{{-- {{route('admin.restaurant.show')}} --}}">
+    <div class="card" style="width: 18rem;">
+        <img src="{{ asset('storage/' . $restaurant->image) }}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h2 class="card-text">{{$restaurant->name}}</h2>
+        </div>
     </div>
-</div>
+</a>
 @else
     @include('generals.botons.create_boton', ['route' => route('admin.restaurant.create'), 'add' => 'Register your Restaurant'])
 @endif

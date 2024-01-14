ba8632f50f6b3337f8d42584ecf940e1e63bcf26
silviapetrino <petrino.silvia@gmail.com>
2024-01-14 15:12:54 +0100

    [fix] index restaurant

--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -7,10 +7,10 @@
 {{-- @dd($restaurant) --}}
     @foreach ( $restaurant as $item )
 
-        @if (!isset($restaurant->user_id))
+        @if (isset($item->user_id))
         <h2>{{ $item->name }}</h2>
         @else
-        <h2>ti odio</h2>
+        <h2>no restaurant</h2>
         @endif
 
     @endforeach

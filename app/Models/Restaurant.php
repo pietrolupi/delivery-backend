ab08d6b647ca283c0fbd77da4e74bdbb5b2497c9
franedsandi <franedsandi@gmail.com>
2024-01-15 12:28:22 +0100

    [fixed] order deleted with delete restaurant

--- app/Models/Restaurant.php
+++ app/Models/Restaurant.php
@@ -1,14 +1,23 @@
 <?php
 
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\User;
 
+
 class Restaurant extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'name',
+        'address',
+        'image',
+    ];
 
     public function user(){
         return $this->belongsTo(User::class);
@@ -22,11 +31,16 @@ class Restaurant extends Model
         return $this->hasMany(Product::class);
     }
 
-    protected $fillable = [
-        'user_id',
-        'name',
-        'address',
-        'image',
-    ];
+    public function orders(){
+        return $this->hasManyThrough(Order::class, OrderProduct::class, 'product_id', 'id', 'id', 'order_id')
+            ->distinct(); // Adding distinct to avoid duplicate orders when multiple products have the same order
+    }
 
+    protected static function booted()
+    {
+        static::deleting(function ($restaurant) {
+            // Delete all orders associated with the restaurant
+            $restaurant->orders()->delete();
+        });
+    }
 }

dac1e2e27cc8bb4a6c33ea417e8df552d6cd5c6c
erikvella <vella.erik1@gmail.com>
2024-01-16 18:32:57 +0100

    [fix] ProductRequest (image)

--- app/Http/Requests/ProductRequest.php
+++ app/Http/Requests/ProductRequest.php
@@ -26,7 +26,7 @@ class ProductRequest extends FormRequest
             "ingredients" => "required|min:2|max:255",
             "description" => "max:400",
             "price"=> "required|numeric|between:0,9999",
-            "image"=> "image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048'",
+            "image"=> "image|mimes:jpeg,png,jpg,gif,svg,webp|max:2048",
 
         ];
 

58bae8534dadfc4fd39236ed2cadae30c45794bd
silviapetrino <petrino.silvia@gmail.com>
2024-01-16 15:24:18 +0100

    [remove]: restaurant crud useless files

--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     @include('generals.partials.sessions')
-    
-    @if (isset($restaurant->user_id))
+
+
     <h2>Your restaurant:</h2>
 
         <a href="{{ route('admin.restaurant.show', $restaurant) }}" class="text-decoration-none">
@@ -14,11 +14,5 @@
                 </div>
             </div>
         </a>
-    @else
-    <h2>Register your restaurant:</h2>
-        @include('generals.buttons.register_restaurant_btn', [
-            'route' => route('admin.restaurant.create'),
-            'add' => 'Register your Restaurant',
-        ])
-    @endif
+
 @endsection

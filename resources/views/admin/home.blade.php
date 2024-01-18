b02bb72d3e577358a1f6ae4221446956d6843049
silviapetrino <petrino.silvia@gmail.com>
2024-01-18 18:19:55 +0100

    [feat]: style in homepage

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,6 +1,15 @@
 @extends('layouts.admin')
 
 @section('content')
+<div class="m-3 home content d-flex flex-wrap flex-md-row justify-content-between p-4 rounded-2">
     <h1>Welcome in DeliveBoo</h1>
-    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem pariatur facere praesentium odio natus nemo molestias provident, placeat beatae eligendi quos tenetur enim est sint, a voluptatibus. Inventore omnis doloribus amet ex suscipit tenetur explicabo aspernatur. Perspiciatis saepe ipsa eum temporibus sapiente vel nesciunt, consequatur, distinctio at commodi, porro explicabo!</p>
+
+    <p class="text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem pariatur facere praesentium odio natus nemo molestias provident, placeat beatae eligendi quos tenetur enim est sint, a voluptatibus. Inventore omnis doloribus amet ex suscipit tenetur explicabo aspernatur. Perspiciatis saepe ipsa eum temporibus sapiente vel nesciunt, consequatur, distinctio at commodi, porro explicabo!
+    </p>
+    <div class="image">
+        <img class="w-100 h-100 object-fit-cover" src="/img/DeliveBoo-img.png" alt="logo">
+    </div>
+</div>
+
 @endsection
